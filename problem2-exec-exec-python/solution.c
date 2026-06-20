9d214c84067458c9c9b5509a5496e3493e2c79e3
vicxriffi <vika.razumova2017@yandex.ru>
2026-06-20 15:55:43 +0300

    Update solution.c

--- problem2-exec-exec-python/solution.c
+++ problem2-exec-exec-python/solution.c
@@ -8,10 +8,15 @@
 // Использовать дополнительные процессы запрещено — нужно использовать exec.
 
 int main(void) {
-    // TODO: прочитайте выражение из stdin,
-    //       затем вызовите execvp/execlp для запуска python3,
-    //       который вычислит и выведет результат.
-    //       Подсказка: python3 -c "print(<выражение>)"
+    execlp(
+        "python3",
+        "python3",
+        "-c",
+        "import sys; print(eval(sys.stdin.read()))",
+        (char *)NULL
+    );
+
+    perror("execlp");
 
     return 0;
 }
