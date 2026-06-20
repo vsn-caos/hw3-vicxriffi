09ff7ad5e541e08fb824c4c65fa5858a233a3687
vicxriffi <vika.razumova2017@yandex.ru>
2026-06-20 16:04:13 +0300

    Update solution.c

--- problem3-pipe-connect-2-processes/solution.c
+++ problem3-pipe-connect-2-processes/solution.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 // Программе передаётся два аргумента: CMD1 и CMD2.
 // Необходимо запустить два процесса, выполняющих эти команды,
@@ -9,16 +10,72 @@
 // Эквивалентно: CMD1 | CMD2
 // Родительский процесс должен завершаться самым последним.
 
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <CMD1> <CMD2>\n", argv[0]);
         return 1;
     }
 
-    // TODO: создайте канал (pipe),
-    //       запустите CMD1 (argv[1]) так, чтобы его stdout → write-конец канала,
-    //       запустите CMD2 (argv[2]) так, чтобы его stdin  ← read-конец канала,
-    //       дождитесь завершения обоих дочерних процессов.
+    int pipefd[2];
 
-    return 0;
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        return 1;
+    }
+
+    pid_t first = fork();
+
+    if (first == -1) {
+        perror("fork");
+        close(pipefd[0]);
+        close(pipefd[1]);
+        return 1;
+    }
+
+    if (first == 0) {
+        if (dup2(pipefd[1], STDOUT_FILENO) == -1) {
+            perror("dup2");
+            _exit(1);
+        }
+
+        close(pipefd[0]);
+        close(pipefd[1]);
+
+        execlp(argv[1], argv[1], (char *)NULL);
+
+        perror("execlp CMD1");
+        _exit(1);
+    }
+
+    pid_t second = fork();
+
+    if (second == -1) {
+        perror("fork");
+        close(pipefd[0]);
+        close(pipefd[1]);
+        wait_for(first);
+        return 1;
+    }
+
+    if (second == 0) {
+        if (dup2(pipefd[0], STDIN_FILENO) == -1) {
+            perror("dup2");
+            _exit(1);
+        }
+
+        close(pipefd[0]);
+        close(pipefd[1]);
+
+        execlp(argv[2], argv[2], (char *)NULL);
+
+        perror("execlp CMD2");
+        _exit(1);
+    }
+    close(pipefd[0]);
+    close(pipefd[1]);
+
+    int first_status = wait_for(first);
+    int second_status = wait_for(second);
+    return first_status != 0 ? first_status : second_status;
 }
